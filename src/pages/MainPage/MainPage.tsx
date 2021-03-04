271f797d60962791935ac8fbd1dad46eda9cb1bd
lerakry8 <lerakrya8@gmail.com>
2021-03-04 15:51:10 +0300

    add right menu

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,27 +1,6 @@
 import { JSX } from 'jsx/jsx';
-import { logoutUser } from '../../actions/registration';
-import { Link } from '../../components/Link/Link';
-import { LINKS } from '../../constants/router';
-
-import './style.scss';
+import { MainPageForm } from '../../modules/MainPageForm/MainPageForm';
 
 export const MainPage = () => {
-    console.log(document.cookie);
-
-    return (
-        <div class={'error-page-wrapper'}>
-            <div class={'error-page'}>
-                {'Главная'}
-                <Link to={LINKS.auth} text={'Авторизация'} />
-                <Link to={LINKS.reg} text={'Регистрация'} />
-                <div
-                    onclick={() => {
-                        logoutUser().then((res) => console.log(res));
-                    }}
-                >
-                    {'Выйти'}
-                </div>
-            </div>
-        </div>
-    );
+    return <MainPageForm />;
 };
