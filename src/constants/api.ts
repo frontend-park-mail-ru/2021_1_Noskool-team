b4e2010c6147fd881fbd3d4ee2e2fbbfaf30194d
CUzkov <UskovDanek@gmail.com>
2021-03-09 23:36:16 +0300

    Test cors

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
-export const HOST = '127.0.0.1:8082';
+export const HOST = 'http://127.0.0.1:8082';
 
 export const REGISTER_USER = HOST + '/api/v1/registrate';
 export const AUTH_USER = HOST + '/api/v1/login';
