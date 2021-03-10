d86b31a91b5fc30d483edd90d5cf37bc4998f4cf
CUzkov <UskovDanek@gmail.com>
2021-03-10 18:56:14 +0300

    Add readme

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,6 +1,7 @@
 const DEBUG = true;
 
 export const HOST = DEBUG ? 'http://127.0.0.1:8082' : 'http://178.154.245.200:8082';
+export const HOST_IMG = DEBUG ? 'http://127.0.0.1:8082/api/v1/data/' : 'http://178.154.245.200:8082/api/v1/data/';
 
 export const REGISTER_USER = HOST + '/api/v1/registrate';
 export const AUTH_USER = HOST + '/api/v1/login';
