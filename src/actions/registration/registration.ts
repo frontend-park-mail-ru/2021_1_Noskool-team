309e85b885f475ea0cd4070c88659178748a3263
CUzkov <UskovDanek@gmail.com>
2021-03-10 17:19:10 +0300

    Add logout

--- src/actions/registration/registration.ts
+++ src/actions/registration/registration.ts
@@ -1,6 +1,8 @@
 import { RegisterUser, AuthUser } from '../../types/registration';
 import { post, get } from '../common/common';
 import { REGISTER_USER, AUTH_USER, LOGOUT_USER } from '../../constants/api';
+import { redirectTo } from '../../utils/router';
+import { LINKS } from '../../constants/router';
 
 export const registerUser = (body: RegisterUser) => {
     return post(REGISTER_USER, body);
@@ -10,6 +12,10 @@ export const authUser = (body: AuthUser) => {
     return post(AUTH_USER, body);
 };
 
-export const logoutUser = () => {
-    return get(LOGOUT_USER);
+export const logoutUser = async () => {
+    const response = await get(LOGOUT_USER);
+    if (response.status === 401) {
+        redirectTo(LINKS.auth);
+    }
+    return response;
 };
