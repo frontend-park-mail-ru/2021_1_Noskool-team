b21b092ab75b250095434967ccac3523414747f4
lerakry8 <lerakrya8@gmail.com>
2021-04-11 22:07:24 +0300

    обавила проверку на csrf

--- src/actions/user/user.constants.ts
+++ src/actions/user/user.constants.ts
@@ -3,3 +3,4 @@ import { HOST } from '../../constants/api';
 export const PROFILE = HOST + '/api/v1/profile';
 export const CHANGE_PROFILE = HOST + '/api/v1/profile/update';
 export const CHANGE_USER_PHOTE = HOST + '/api/v1/profile/avatar/upload';
+export const GET_CSRF = HOST + '/api/v1/profile/csrf';
