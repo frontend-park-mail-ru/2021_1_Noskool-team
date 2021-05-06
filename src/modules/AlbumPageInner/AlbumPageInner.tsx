98d443756c3f86d9f3223d267e7f70f6f2b92f3f
CUzkov <UskovDanek@gmail.com>
2021-05-06 20:08:35 +0300

    Fix tracks

--- src/modules/AlbumPageInner/AlbumPageInner.tsx
+++ src/modules/AlbumPageInner/AlbumPageInner.tsx
@@ -48,7 +48,6 @@ export const AlbumPageInner = () => {
     const id = window.location.pathname.split('/');
 
     if (Number(id[id.length - 1]) !== previosId) {
-        albumPageStore.album.tracks = [];
         previosId = Number(id[id.length - 1]);
         getAlbumById(Number(id[id.length - 1]));
     }
