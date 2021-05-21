34b0864b645b9897c72dafec3185b66a8e9bc429
CUzkov <UskovDanek@gmail.com>
2021-05-21 10:54:53 +0300

    Fix mobile

--- src/actions/common/common.ts
+++ src/actions/common/common.ts
@@ -96,7 +96,7 @@ export const getMainPage = async <P>(url: string): Promise<P | undefined> => {
         }
         return response;
     } catch (e) {
-        console.log(`да лол, обнови браузер, ошибочка: ${e}`);
+        alert(`да лол, обнови браузер, ошибочка: ${e}`);
     }
 };
 
