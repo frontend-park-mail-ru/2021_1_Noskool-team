069cc22d421fbc454103955ea6f396bc8e47a4d9
CUzkov <UskovDanek@gmail.com>
2021-05-21 10:07:27 +0300

    Add files for cd/ci

--- src/constants/api.product.ts
+++ src/constants/api.product.ts
@@ -1,6 +1,6 @@
-export const HOST = 'http://localhost:8082';
-export const HOST_IMG = 'http://localhost:8082';
-export const TRACK_HOST = 'http://localhost:8888';
+export const HOST = 'http://84.201.189.81:8082';
+export const HOST_IMG = 'http://84.201.189.81:8082';
+export const TRACK_HOST = 'http://84.201.189.81:8888';
 
 export const REGISTER_USER = HOST + '/api/v1/registrate';
 export const AUTH_USER = HOST + '/api/v1/login';
