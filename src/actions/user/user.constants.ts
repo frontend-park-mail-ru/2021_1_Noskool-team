451cf29c569f9dd172e21d19873eee9baa08b657
CUzkov <UskovDanek@gmail.com>
2021-05-21 17:15:31 +0300

    another server

--- src/actions/user/user.constants.ts
+++ src/actions/user/user.constants.ts
@@ -1,6 +1,4 @@
-import { HOST } from '../../constants/api';
-
-export const PROFILE = HOST + '/api/v1/profile';
-export const CHANGE_PROFILE = HOST + '/api/v1/profile/update';
-export const CHANGE_USER_PHOTE = HOST + '/api/v1/profile/avatar/upload';
-export const GET_CSRF = HOST + '/api/v1/profile/csrf';
+export const PROFILE = '/api/v1/profile';
+export const CHANGE_PROFILE = '/api/v1/profile/update';
+export const CHANGE_USER_PHOTE = '/api/v1/profile/avatar/upload';
+export const GET_CSRF = '/api/v1/profile/csrf';
