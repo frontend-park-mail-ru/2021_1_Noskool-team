d74e9af997f94276a32f6d5cc59266424f28f04a
CUzkov <UskovDanek@gmail.com>
2021-05-21 13:18:06 +0300

    another server

--- src/constants/api.product.ts
+++ src/constants/api.product.ts
@@ -1,6 +1,6 @@
-export const HOST = 'http://84.201.189.81:8082';
-export const HOST_IMG = 'http://84.201.189.81:8082';
-export const TRACK_HOST = 'http://84.201.189.81:8888';
+export const HOST = 'http://178.154.253.38:8082';
+export const HOST_IMG = 'http://178.154.253.38:8082';
+export const TRACK_HOST = 'http://178.154.253.38:8888';
 
 export const REGISTER_USER = HOST + '/api/v1/registrate';
 export const AUTH_USER = HOST + '/api/v1/login';
