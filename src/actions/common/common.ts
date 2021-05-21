6ef3705db162d92a084597a69e0c37adf70cb013
CUzkov <UskovDanek@gmail.com>
2021-05-21 12:36:17 +0300

    Test ci

--- src/actions/common/common.ts
+++ src/actions/common/common.ts
@@ -93,7 +93,7 @@ export const getMainPage = async <P>(url: string): Promise<P | undefined> => {
         }
         return response;
     } catch (e) {
-        console.log(`да лол, обнови браузер, ошибочка: ${e}`);
+        console.log(`Да лол, обнови браузер, ошибочка: ${e}`);
     }
 };
 
